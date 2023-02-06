7c9d6cd735ebb379d71076692c4e39d804590e0d
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-02-06 14:39:23 +0530

    Update JCheckBoxDemo.java

--- Week 14 - Swing Componenets/JCheckBoxDemo.java
+++ Week 14 - Swing Componenets/JCheckBoxDemo.java
@@ -1,4 +1,4 @@
-// Demonstration of JCheckBox
+// Demonstration of JCheckBox:
 
 import javax.swing.*;  
 
@@ -11,15 +11,15 @@ public class JCheckBoxDemo
         System.out.println("Enter your preferred Language: ");
 
         JCheckBox CheckBox1 = new JCheckBox("C++", false);  
-        CheckBox1.setBounds(100,100, 200,50);  
+        CheckBox1.setBounds(100,50, 200,50);  
 
         JCheckBox CheckBox2 = new JCheckBox("Java", false);  
-        CheckBox2.setBounds(100,150, 200,50);  
+        CheckBox2.setBounds(100,100, 200,50);  
 
         f.add(CheckBox1);  
         f.add(CheckBox2);  
 
-        f.setSize(400,400);  
+        f.setSize(300,300);  
         f.setLayout(null);  
         f.setVisible(true);  
     }  
